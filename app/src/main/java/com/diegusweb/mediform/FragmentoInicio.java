50460d7b17d31f2b530491d33162e87f289a469c
Diego Rueda <diego.rueda2@gmail.com>
2015-12-03 16:38:00 -0400

    lista inicial con mockups

---
+++ app/src/main/java/com/diegusweb/mediform/FragmentoInicio.java
@@ -0,0 +1,49 @@
+package com.diegusweb.mediform;
+
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.diegusweb.mediform.adapters.AdaptadorInicio;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentoInicio extends Fragment {
+
+
+    private RecyclerView reciclador;
+    private LinearLayoutManager layoutManager;
+    private AdaptadorInicio adaptador;
+
+    public FragmentoInicio() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragmento_inicio, container, false);
+
+        View view = inflater.inflate(R.layout.fragmento_inicio, container, false);
+
+        reciclador = (RecyclerView) view.findViewById(R.id.reciclador);
+        layoutManager = new LinearLayoutManager(getActivity());
+        reciclador.setLayoutManager(layoutManager);
+
+        adaptador = new AdaptadorInicio();
+        reciclador.setAdapter(adaptador);
+        return view;
+    }
+
+
+}
--- app/src/main/java/com/diegusweb/mediform/MainActivity.java
+++ app/src/main/java/com/diegusweb/mediform/MainActivity.java
@@ -1,5 +1,8 @@
 package com.diegusweb.mediform;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -21,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         agregarToolbar();
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        if (navigationView != null) {
+            prepararDrawer(navigationView);
+            // Seleccionar item por defecto
+            seleccionarItem(navigationView.getMenu().getItem(0));
+        }
     }
 
     private void agregarToolbar() {
@@ -50,5 +60,48 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void prepararDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        seleccionarItem(menuItem);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+    }
+
+    private void seleccionarItem(MenuItem itemDrawer) {
+        Fragment fragmentoGenerico = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch (itemDrawer.getItemId()) {
+            case R.id.item_inicio:
+                fragmentoGenerico = new FragmentoInicio();
+                break;
+            case R.id.item_cuenta:
+                // Fragmento para la sección Cuenta
+                break;
+            case R.id.item_categorias:
+                // Fragmento para la sección Categorías
+                break;
+            case R.id.item_configuracion:
+                // Iniciar actividad de configuración
+                break;
+        }
+        if (fragmentoGenerico != null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.contenedor_principal, fragmentoGenerico)
+                    .commit();
+        }
+
+        // Setear título actual
+        setTitle(itemDrawer.getTitle());
     }
 }
